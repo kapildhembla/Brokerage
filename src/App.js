bba87df6ff8dc3ce48b3506519e1ef990df1db88
Kapil <53132870+kapildhembla@users.noreply.github.com>
2019-10-29 11:27:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import ViewTrades from './component/ViewTrades';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ClientTrade from './component/ClientTrade';
+
+function App() {
+  return (
+    <ViewTrades/>
+    //<ClientTrade/>
+  );
+}
+
+export default App;
